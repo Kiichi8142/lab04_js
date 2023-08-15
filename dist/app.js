fe056eecb91b9d80cba2bdfa3b86f4615c5cf40d
Koharu2568 <vponsuwan@gmail.com>
2023-08-15 20:26:27 +0700

    add items counter

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,5 @@
 currentSlideOrder = 1;
+productList = [0, 0, 0, 0]
 
 function changeImg() {
     let carousel = document.getElementById("carousel");
@@ -16,5 +17,24 @@ function prevImg() {
     if (currentSlideOrder > 1 && currentSlideOrder <= 4) {
         currentSlideOrder--;
         changeImg();
+    }
+}
+
+function updateItems() {
+    document.getElementById('item-0-counter').innerHTML = productList[0];
+    document.getElementById('item-1-counter').innerHTML = productList[1];
+    document.getElementById('item-2-counter').innerHTML = productList[2];
+    document.getElementById('item-3-counter').innerHTML = productList[3];
+}
+
+function increaseItem(productId) {
+    productList[productId] += 1;
+    updateItems()
+}
+
+function decreaseItem(productId) {
+    if (productList[productId] != 0) {
+        productList[productId] -= 1;
+        updateItems()
     }
 }
