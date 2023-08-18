81e9bfa505d8e9a4a16b4bf0d881073553742b27
Koharu2568 <vponsuwan@gmail.com>
2023-08-18 22:04:52 +0700

    add portfolio chart

---
+++ dist/portfolio.js
@@ -0,0 +1,527 @@
+const monthly_playcounts = [
+    {
+      "start_date": "2013-05-01",
+      "count": 126
+    },
+    {
+      "start_date": "2013-06-01",
+      "count": 598
+    },
+    {
+      "start_date": "2013-07-01",
+      "count": 518
+    },
+    {
+      "start_date": "2013-08-01",
+      "count": 425
+    },
+    {
+      "start_date": "2013-09-01",
+      "count": 144
+    },
+    {
+      "start_date": "2013-10-01",
+      "count": 59
+    },
+    {
+      "start_date": "2013-11-01",
+      "count": 73
+    },
+    {
+      "start_date": "2013-12-01",
+      "count": 129
+    },
+    {
+      "start_date": "2014-01-01",
+      "count": 216
+    },
+    {
+      "start_date": "2014-02-01",
+      "count": 130
+    },
+    {
+      "start_date": "2014-04-01",
+      "count": 11
+    },
+    {
+      "start_date": "2014-05-01",
+      "count": 1
+    },
+    {
+      "start_date": "2014-07-01",
+      "count": 6
+    },
+    {
+      "start_date": "2014-08-01",
+      "count": 39
+    },
+    {
+      "start_date": "2015-02-01",
+      "count": 622
+    },
+    {
+      "start_date": "2015-03-01",
+      "count": 1440
+    },
+    {
+      "start_date": "2015-04-01",
+      "count": 1065
+    },
+    {
+      "start_date": "2015-05-01",
+      "count": 1074
+    },
+    {
+      "start_date": "2015-06-01",
+      "count": 1208
+    },
+    {
+      "start_date": "2015-07-01",
+      "count": 1015
+    },
+    {
+      "start_date": "2015-08-01",
+      "count": 1091
+    },
+    {
+      "start_date": "2015-09-01",
+      "count": 669
+    },
+    {
+      "start_date": "2015-10-01",
+      "count": 1001
+    },
+    {
+      "start_date": "2015-11-01",
+      "count": 996
+    },
+    {
+      "start_date": "2015-12-01",
+      "count": 878
+    },
+    {
+      "start_date": "2016-01-01",
+      "count": 757
+    },
+    {
+      "start_date": "2016-02-01",
+      "count": 1011
+    },
+    {
+      "start_date": "2016-03-01",
+      "count": 1442
+    },
+    {
+      "start_date": "2016-04-01",
+      "count": 889
+    },
+    {
+      "start_date": "2016-05-01",
+      "count": 667
+    },
+    {
+      "start_date": "2016-06-01",
+      "count": 674
+    },
+    {
+      "start_date": "2016-07-01",
+      "count": 646
+    },
+    {
+      "start_date": "2016-08-01",
+      "count": 1143
+    },
+    {
+      "start_date": "2016-09-01",
+      "count": 1271
+    },
+    {
+      "start_date": "2016-10-01",
+      "count": 568
+    },
+    {
+      "start_date": "2016-11-01",
+      "count": 363
+    },
+    {
+      "start_date": "2016-12-01",
+      "count": 657
+    },
+    {
+      "start_date": "2017-01-01",
+      "count": 512
+    },
+    {
+      "start_date": "2017-02-01",
+      "count": 651
+    },
+    {
+      "start_date": "2017-03-01",
+      "count": 590
+    },
+    {
+      "start_date": "2017-04-01",
+      "count": 480
+    },
+    {
+      "start_date": "2017-05-01",
+      "count": 445
+    },
+    {
+      "start_date": "2017-06-01",
+      "count": 403
+    },
+    {
+      "start_date": "2017-07-01",
+      "count": 388
+    },
+    {
+      "start_date": "2017-08-01",
+      "count": 295
+    },
+    {
+      "start_date": "2017-09-01",
+      "count": 434
+    },
+    {
+      "start_date": "2017-10-01",
+      "count": 685
+    },
+    {
+      "start_date": "2017-11-01",
+      "count": 284
+    },
+    {
+      "start_date": "2017-12-01",
+      "count": 367
+    },
+    {
+      "start_date": "2018-01-01",
+      "count": 574
+    },
+    {
+      "start_date": "2018-02-01",
+      "count": 471
+    },
+    {
+      "start_date": "2018-03-01",
+      "count": 215
+    },
+    {
+      "start_date": "2018-04-01",
+      "count": 216
+    },
+    {
+      "start_date": "2018-05-01",
+      "count": 798
+    },
+    {
+      "start_date": "2018-06-01",
+      "count": 641
+    },
+    {
+      "start_date": "2018-07-01",
+      "count": 151
+    },
+    {
+      "start_date": "2018-08-01",
+      "count": 312
+    },
+    {
+      "start_date": "2018-09-01",
+      "count": 249
+    },
+    {
+      "start_date": "2018-10-01",
+      "count": 464
+    },
+    {
+      "start_date": "2018-11-01",
+      "count": 653
+    },
+    {
+      "start_date": "2018-12-01",
+      "count": 408
+    },
+    {
+      "start_date": "2019-01-01",
+      "count": 390
+    },
+    {
+      "start_date": "2019-02-01",
+      "count": 1510
+    },
+    {
+      "start_date": "2019-03-01",
+      "count": 510
+    },
+    {
+      "start_date": "2019-04-01",
+      "count": 800
+    },
+    {
+      "start_date": "2019-05-01",
+      "count": 342
+    },
+    {
+      "start_date": "2019-06-01",
+      "count": 111
+    },
+    {
+      "start_date": "2019-07-01",
+      "count": 125
+    },
+    {
+      "start_date": "2019-08-01",
+      "count": 133
+    },
+    {
+      "start_date": "2019-09-01",
+      "count": 126
+    },
+    {
+      "start_date": "2019-10-01",
+      "count": 337
+    },
+    {
+      "start_date": "2019-11-01",
+      "count": 85
+    },
+    {
+      "start_date": "2019-12-01",
+      "count": 129
+    },
+    {
+      "start_date": "2020-01-01",
+      "count": 715
+    },
+    {
+      "start_date": "2020-02-01",
+      "count": 648
+    },
+    {
+      "start_date": "2020-03-01",
+      "count": 179
+    },
+    {
+      "start_date": "2020-04-01",
+      "count": 442
+    },
+    {
+      "start_date": "2020-05-01",
+      "count": 130
+    },
+    {
+      "start_date": "2020-06-01",
+      "count": 170
+    },
+    {
+      "start_date": "2020-07-01",
+      "count": 473
+    },
+    {
+      "start_date": "2020-08-01",
+      "count": 1356
+    },
+    {
+      "start_date": "2020-09-01",
+      "count": 1001
+    },
+    {
+      "start_date": "2020-10-01",
+      "count": 343
+    },
+    {
+      "start_date": "2020-11-01",
+      "count": 496
+    },
+    {
+      "start_date": "2020-12-01",
+      "count": 1467
+    },
+    {
+      "start_date": "2021-01-01",
+      "count": 748
+    },
+    {
+      "start_date": "2021-02-01",
+      "count": 248
+    },
+    {
+      "start_date": "2021-03-01",
+      "count": 303
+    },
+    {
+      "start_date": "2021-04-01",
+      "count": 325
+    },
+    {
+      "start_date": "2021-05-01",
+      "count": 606
+    },
+    {
+      "start_date": "2021-06-01",
+      "count": 398
+    },
+    {
+      "start_date": "2021-07-01",
+      "count": 242
+    },
+    {
+      "start_date": "2021-08-01",
+      "count": 182
+    },
+    {
+      "start_date": "2021-09-01",
+      "count": 26
+    },
+    {
+      "start_date": "2021-11-01",
+      "count": 244
+    },
+    {
+      "start_date": "2021-12-01",
+      "count": 454
+    },
+    {
+      "start_date": "2022-01-01",
+      "count": 54
+    },
+    {
+      "start_date": "2022-02-01",
+      "count": 115
+    },
+    {
+      "start_date": "2022-03-01",
+      "count": 420
+    },
+    {
+      "start_date": "2022-04-01",
+      "count": 319
+    },
+    {
+      "start_date": "2022-05-01",
+      "count": 263
+    },
+    {
+      "start_date": "2022-06-01",
+      "count": 298
+    },
+    {
+      "start_date": "2022-07-01",
+      "count": 231
+    },
+    {
+      "start_date": "2022-08-01",
+      "count": 584
+    },
+    {
+      "start_date": "2022-09-01",
+      "count": 122
+    },
+    {
+      "start_date": "2022-10-01",
+      "count": 291
+    },
+    {
+      "start_date": "2022-11-01",
+      "count": 333
+    },
+    {
+      "start_date": "2022-12-01",
+      "count": 286
+    },
+    {
+      "start_date": "2023-01-01",
+      "count": 858
+    },
+    {
+      "start_date": "2023-02-01",
+      "count": 404
+    },
+    {
+      "start_date": "2023-03-01",
+      "count": 748
+    },
+    {
+      "start_date": "2023-04-01",
+      "count": 1172
+    },
+    {
+      "start_date": "2023-05-01",
+      "count": 1472
+    },
+    {
+      "start_date": "2023-06-01",
+      "count": 1030
+    },
+    {
+      "start_date": "2023-07-01",
+      "count": 379
+    },
+    {
+      "start_date": "2023-08-01",
+      "count": 67
+    }
+]
+
+document.addEventListener("DOMContentLoaded", function(){
+    let ctx = document.getElementById('myChart').getContext('2d');
+
+    // Define the data for the chart
+    let data = {
+        labels: ['Python', 'Lua', 'JS', 'C/C++', 'C#'],
+        datasets: [{
+            label: 'Programming Skills',
+            backgroundColor: [
+                'rgb(255, 99, 132)',   // Python
+                'rgb(54, 162, 235)',   // Lua
+                'rgb(255, 205, 86)',   // JS (JavaScript)
+                'rgb(75, 192, 192)',   // C/C++
+                'rgb(153, 102, 255)'   // C#
+            ],
+            borderWidth: 1,
+            data: [95, 70, 80, 50, 25]
+        }]
+    };
+
+    let playdata = {
+        labels: monthly_playcounts.map(row => row.start_date),
+        datasets: [{
+            label: 'osu! Play count monthly',
+            borderWidth: 1,
+            data: monthly_playcounts.map(row => row.count)
+        }]
+    };
+
+    // Create the bar chart
+    let myBarChart = new Chart(ctx, {
+        type: 'bar',
+        data: data,
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+
+    let myBarChart2 = new Chart(document.getElementById('myChart2').getContext('2d'), {
+        type: 'doughnut',
+        data: data,
+        options: {}
+    });
+
+    let myBarChart3 = new Chart(document.getElementById('myChart3').getContext('2d'), {
+        type: 'line',
+        data: playdata,
+        options: {}
+    });
+
+    let myBarChart4 = new Chart(document.getElementById('myChart4').getContext('2d'), {
+        type: 'polarArea',
+        data: data,
+        options: {}
+    });
+})
